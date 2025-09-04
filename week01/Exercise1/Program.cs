2539ecd093e0dc4e9e01cac30dcffeb8889fc9e7
Aaronfonoi <aaronfonoi@gmail.com>
2025-09-03 17:28:15 -0700

    Exercise1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        
+        Console.WriteLine("what is your first name?");
+        string firstname = Console.ReadLine();
+
+        Console.WriteLine("what is your last name?");
+        string lastname = Console.ReadLine();
+        
+        Console.WriteLine($"your name is {lastname}, {firstname} {lastname}.Aaron");
     }
 }
