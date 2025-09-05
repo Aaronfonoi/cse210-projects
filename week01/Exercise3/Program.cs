d6f74c97020ee45bf1dd7e19bbc97b30ba27a113
Aaronfonoi <aaronfonoi@gmail.com>
2025-09-05 09:02:29 -0700

    exercise3 no stretch challenge

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Console.Write("Please enter a magic number.");
+        //string user = Console.ReadLine();
+        //int magicNumber = int.Parse(user);
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+
+        int guess = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("Guess the magic number ");
+            guess = int.Parse(Console.ReadLine());
+        
+
+        
+        
+
+
+
+
+
+                if (magicNumber == guess)
+            {
+                Console.WriteLine("correct");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("too high");
+            }
+            else
+            {
+                Console.WriteLine("Too low");
+            }
+
+                Console.WriteLine($"your guess is: {guess}");
+        }
     }
 }
