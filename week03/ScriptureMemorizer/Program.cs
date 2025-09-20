735ae7bfc5b7b11f825accb4c380280b5945ac84
Aaronfonoi <aaronfonoi@gmail.com>
2025-09-19 18:07:49 -0700

    minor cleanup

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -2,7 +2,7 @@ using System;
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
         
         Scripture scripture = new Scripture("Helaman ", 5, 6,7, "6 Behold, my sons, I desire that ye should remember to keep the commandments of God; and I would that ye should declare unto the people these words. Behold, I have given unto you the names of our first parents who came out of the land of jerusalem; and this I have done that when you remember your names ye may remember them; and when ye remember them ye may remember their works; and when ye remember their works ye may know how that it is said, and also written, that they were good. 7 Therefore, my sons, I would that ye should do that which is good, that it may be said of you, and also written, even as it has been said and written of them.");
