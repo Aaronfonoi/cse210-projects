4d34da781c5b9cb247720cfccee09df757773e4c
Aaronfonoi <aaronfonoi@gmail.com>
2025-10-02 17:55:44 -0700

    Assignments

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Aaron Fonoimoana", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+
+        MathAssignment a2 = new MathAssignment("Aaron Fonoimoana", "Fractions", "8.6", "7-16");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Aaron Fonoimoana", "American History", "The Hero's of world war II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
+        
+
+
+
     }
 }
