83e5211878625fa31f2b02ffc31f95664c036dfb
Aaronfonoi <aaronfonoi@gmail.com>
2025-10-09 19:11:40 -0700

    shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Drawing;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Purple", 3, 6);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("yellow", 7);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"the {color} shape has an area of {area}");
+        }
+
     }
 }
